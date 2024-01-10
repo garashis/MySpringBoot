92277cd70898d219ae00b3fb2c3ea632cb7f448c
Ashish Garg <ashgarg@publicisgroupe.net>
2024-01-10 11:39:20 +0000

    MongoDB and Redis Integration

--- src/main/java/com/example/demo/repository/CustomerRepository.java
+++ src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.repository;
 
 import com.example.demo.dto.Customer;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 
@@ -8,9 +10,12 @@ import java.util.Optional;
 
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
 
-    //@Cacheable(value = "findByFirstName", unless="#result == null")
-    @Cacheable(value = "user", key = "#username")
+    @Cacheable(value = "user", key = "#username", sync = true)
     Optional<Customer> findByUsername(String username);
+
+    @CachePut(value = "user", key = "#customer.username")
+    Customer save(Customer customer);
+    @CacheEvict(value = "user", key = "#username")
     Optional<Customer> deleteByUsername(String username);
 
     //@Cacheable(value = "employee") Employee save(Employee employee);
