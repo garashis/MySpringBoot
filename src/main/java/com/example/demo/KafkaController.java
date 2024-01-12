42417c23752842e0b9dd3e34803edb54ee16c48d
Ashish Garg <ashgarg@publicisgroupe.net>
2024-01-12 14:20:33 +0000

    Added Kakfa consumer

--- src/main/java/com/example/demo/KafkaController.java
+++ src/main/java/com/example/demo/KafkaController.java
@@ -1,24 +1,23 @@
 package com.example.demo;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class KafkaController {
+    public static final String TOPIC_NAME = "myTest";
 
-    private final KafkaSenderExample kafkaSenderExample;
-
-    @GetMapping("/sendMessage")
-    void sendMessageWithCallback() {
-        String message = "hello";
-        ListenableFuture<SendResult<String, String>> future =
-                kafkaSenderExample.sendMessage(message, "quickstart-events");
-
+    private final KafkaProducer kafkaProducer;
+    @GetMapping("/publish")
+    public ResponseEntity<String> publish(@RequestParam("message") String message){
+        ListenableFuture<SendResult<String, String>> future = kafkaProducer.sendMessage(TOPIC_NAME, message);
         future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
             @Override
             public void onSuccess(SendResult<String, String> result) {
@@ -34,5 +33,7 @@ public class KafkaController {
                         ex.getMessage());
             }
         });
+
+        return ResponseEntity.ok("Message sent to kafka topic: " + message);
     }
 }
